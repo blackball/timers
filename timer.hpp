36f77b9192719e5804f5ea54936b5d732ee91b11
Hui <hui.li@daqri.com>
2017-10-09 10:40:10 +0200

    add a scoped version of timer

--- timer.hpp
+++ timer.hpp
@@ -6,11 +6,18 @@
 class Timer {
     std::chrono::high_resolution_clock::time_point _start, _stop;    
 public:
-    inline void start() {_start = std::chrono::high_resolution_clock::now();}
-    inline void stop() {_stop = std::chrono::high_resolution_clock::now();}
+    inline void start() { _start = std::chrono::high_resolution_clock::now(); }
+    inline void stop() { _stop = std::chrono::high_resolution_clock::now(); }
     inline float get_duration_ms() const {
         return std::chrono::duration_cast<std::chrono::milliseconds>(_stop - _start).count();
     }
+};
+
+class ScopedTimer {
+    std::chrono::high_resolution_clock::time_point _start    
+public:
+    ScopedTimer() { _start = std::chrono::high_resolution_clock::now(); }
+    ~ScopedTimer(){ std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - _start).count(); }
 };
 
 #endif /* CPP_TIMER_H */
